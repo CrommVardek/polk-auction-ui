4091d7dd2b3d8e630e0fcd511fb67d0c53f7b99c
CrommVardek <cyrilcarlier@hotmail.com>
2021-11-29 21:38:52 +0100

    Adding test dependencies and structure

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appNameElement = screen.getByText(/PolkAuction/i);
+  expect(appNameElement).toBeInTheDocument();
 });

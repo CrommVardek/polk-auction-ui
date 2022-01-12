2b4f9de66fce41b08ef84f54d8be3e7ff7f9c561
CrommVardek <cyrilcarlier@hotmail.com>
2022-01-12 23:02:37 +0100

    Add about page information

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -8,7 +8,27 @@ export const AboutPage = () => {
       <div className='about-page-content'>
         <p>
           Polk-Auction is a website that allows you to have a concise, simple an easy to understand view of the
-          Parachain, auction and crowdloan state of the Dotsama's ecosystem.
+          parachain, auction and crowdloan state of the Dotsama's ecosystem. We focus on on-chain data with some useful
+          and relevant off-chain information surrounding the state of Polkadot / Kusama.
+        </p>
+      </div>
+      <br />
+      <h3>
+        What is <i>not</i> PolkAuction ?
+      </h3>
+      <div className='about-page-content'>
+        <p>
+          Polk-Auction is <i>not</i> an information page about the parachain's projects. Aside from a few references to
+          official's parachain websites, we believe that it is important for you to do your own research (DYOR).
+        </p>
+      </div>
+      <br />
+      <h3>Have a suggestion, idea or found a bug ?</h3>
+      <div className='about-page-content'>
+        <p>
+          Suggestion from our users is important, you can share your ideas by creating an issue on our{' '}
+          <a href='https://github.com/CrommVardek/polk-auction-ui'>github repository</a>. If you find a bug, you can
+          report it <a href='https://github.com/CrommVardek/polk-auction-ui/issues/new'>here</a>.
         </p>
       </div>
     </div>

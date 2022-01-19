69916dea65846d201ebb16f54a74430af2b757fd
CrommVardek <cyrilcarlier@hotmail.com>
2022-01-19 22:16:16 +0100

    add tooltip with leaseperiod start and end date for crowdloan

--- src/utils/LeasePeriodUtils.ts
+++ src/utils/LeasePeriodUtils.ts
@@ -1,6 +1,10 @@
 import { LeasePeriod } from '../polk-auction-api/models/Lease';
 import moment from 'moment';
 
+export const timeStampToDateFormattedString = (timeStamp: number) => {
+  return moment(new Date(timeStamp)).utc().format('DD-MM-YYYY h:mm:ss a');
+};
+
 export const getMaxEndDateLeasePeriod = (leasePeriods: LeasePeriod[] | undefined) => {
   console.log(leasePeriods);
   if (leasePeriods !== undefined && leasePeriods.length > 0) {
@@ -9,9 +13,7 @@ export const getMaxEndDateLeasePeriod = (leasePeriods: LeasePeriod[] | undefined
       {} as LeasePeriod,
     )?.endTimeStamp;
     console.log(timeStamp);
-    return moment(new Date(timeStamp! as number))
-      .utc()
-      .format('DD-MM-YYYY h:mm:ss a');
+    return timeStampToDateFormattedString(timeStamp! as number);
   }
   return '';
 };
@@ -22,9 +24,7 @@ export const getMinStartDateLeasePeriod = (leasePeriods: LeasePeriod[] | undefin
       (prev, curr) => (prev.startTimeStamp! < curr.startTimeStamp! ? prev : curr),
       {} as LeasePeriod,
     )?.startTimeStamp;
-    return moment(new Date(timeStamp! as number))
-      .utc()
-      .format('DD-MM-YYYY h:mm:ss a');
+    return timeStampToDateFormattedString(timeStamp! as number);
   }
   return '';
 };
